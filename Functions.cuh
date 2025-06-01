091f67756ff54142611bd11a2363c4f771ea81a2
Yasin Eren <yasin@pars-connect.com>
2025-06-01 13:50:00 +0300

    Add Matmul => Functions

--- Functions.cuh
+++ Functions.cuh
@@ -14,6 +14,96 @@ public:
 	static cudnnHandle_t CreateCudnnHandle();
 	static void DestroyCudnnHandle(cudnnHandle_t handle);
 
+	template <typename T, typename U>
+	static void Matmul(cublasHandle_t handle, Tensor<T>& A, Tensor<T>& B, Tensor<U>& C, bool aTrans, bool bTrans, U alpha = 1, U beta = 0)
+	{
+		int m, n, k, lda, ldb, ldc;
+		cublasStatus_t status;
+
+		switch (A.GetDimsize()) {
+		case(2):
+
+			m = bTrans ? B.GetLen(0) : B.GetLen(1);
+			n = C.GetLen(0);
+			k = aTrans ? A.GetLen(0) : A.GetLen(1);
+
+			lda = B.GetStride(1);
+			ldb = A.GetStride(1);
+			ldc = bTrans ? B.GetLen(0) : B.GetLen(1);
+
+			status = cublasGemmEx(
+				handle,
+				bTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
+				aTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
+				m,
+				n,
+				k,
+				&alpha,
+				B.GetData(), B.GetCudaDataType(), lda,
+				A.GetData(), A.GetCudaDataType(), ldb,
+				&beta,
+				C.GetData(), C.GetCudaDataType(), ldc,
+				C.GetCudaDataType(), CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+			break;
+
+		case(3):
+
+			m = bTrans ? B.GetLen(1) : B.GetLen(2);
+			n = C.GetLen(1);
+			k = aTrans ? A.GetLen(1) : A.GetLen(2);
+
+			lda = B.GetStride(1);
+			ldb = A.GetStride(1);
+			ldc = bTrans ? B.GetLen(1) : B.GetLen(2);
+
+			cublasGemmBatchedEx(
+				handle,
+				bTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
+				aTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
+				m,
+				n,
+				k,
+				&alpha,
+				B.GetBatchPtrs(), B.GetCudaDataType(), lda,
+				A.GetBatchPtrs(), A.GetCudaDataType(), ldb,
+				&beta,
+				C.GetBatchPtrs(), C.GetCudaDataType(), ldc,
+				A.GetLen(0), C.GetCudaDataType(), CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+
+			break;
+
+		case(4):
+
+			m = bTrans ? B.GetLen(2) : B.GetLen(3);
+			n = C.GetLen(2);
+			k = aTrans ? A.GetLen(2) : A.GetLen(3);
+
+			lda = B.GetStride(1);
+			ldb = A.GetStride(1);
+			ldc = bTrans ? B.GetLen(2) : B.GetLen(3);
+
+			cublasGemmBatchedEx(
+				handle,
+				bTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
+				aTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
+				m,
+				n,
+				k,
+				&alpha,
+				B.GetBatchPtrs(), B.GetCudaDataType(), lda,
+				A.GetBatchPtrs(), A.GetCudaDataType(), ldb,
+				&beta,
+				C.GetBatchPtrs(), C.GetCudaDataType(), ldc,
+				A.GetLen(0) + A.GetLen(1), C.GetCudaDataType(), CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+
+			break;
+		default:
+			break;
+		}
+
+	}
+
+
 private:
 
 };
--- kernel.cu
+++ kernel.cu
@@ -2,122 +2,31 @@
 #include "Tensor.cuh"
 #include <vector>
 #include <cublas_v2.h>
+#include "functions.cuh"
 
 #pragma comment(lib, "cublas.lib")
 #pragma comment(lib, "cudnn.lib")
-
-template <typename T, typename U>
-void Matmul(cublasHandle_t handle, Tensor<T>& A, Tensor<T>& B, Tensor<U>& C, bool aTrans, bool bTrans, float alpha = 1, float beta = 0)
-{
-	int m, n, k, lda, ldb, ldc;
-	cublasStatus_t status;
-
-	switch (A.GetDimsize()) {
-	case(2):
-
-		m = bTrans ? B.GetLen(0) : B.GetLen(1);
-		n = C.GetLen(0);
-		k = aTrans ? A.GetLen(0) : A.GetLen(1);
-
-		lda = B.GetStride(1);
-		ldb = A.GetStride(1);
-		ldc = bTrans ? B.GetLen(0) : B.GetLen(1);
-
-		status = cublasGemmEx(
-			handle,
-			bTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
-			aTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
-			m,
-			n,
-			k,
-			&alpha,
-			B.GetData(), CUDA_R_8I, lda,
-			A.GetData(), CUDA_R_8I, ldb,
-			&beta,
-			C.GetData(), CUDA_R_32F, ldc,
-			CUDA_R_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
-		break;
-
-	case(3):
-
-		m = bTrans ? B.GetLen(1) : B.GetLen(2);
-		n = C.GetLen(1);
-		k = aTrans ? A.GetLen(1) : A.GetLen(2);
-
-		lda = B.GetStride(1);
-		ldb = A.GetStride(1);
-		ldc = bTrans ? B.GetLen(1) : B.GetLen(2);
-
-		cublasGemmBatchedEx(
-			handle,
-			bTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
-			aTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
-			m,
-			n,
-			k,
-			&alpha,
-			B.GetBatchPtrs(), B.CudaDataType, lda,
-			A.GetBatchPtrs(), A.CudaDataType, ldb,
-			&beta,
-			C.GetBatchPtrs(), C.CudaDataType, ldc,
-			A.GetLen(0), CUBLAS_COMPUTE_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
-
-		break;
-
-	case(4):
-
-		m = bTrans ? B.GetLen(2) : B.GetLen(3);
-		n = C.GetLen(2);
-		k = aTrans ? A.GetLen(2) : A.GetLen(3);
-
-		lda = B.GetStride(1);
-		ldb = A.GetStride(1);
-		ldc = bTrans ? B.GetLen(2) : B.GetLen(3);
-
-		cublasGemmBatchedEx(
-			handle,
-			bTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
-			aTrans ? CUBLAS_OP_T : CUBLAS_OP_N,
-			m,
-			n,
-			k,
-			&alpha,
-			B.GetBatchPtrs(), B.CudaDataType, lda,
-			A.GetBatchPtrs(), A.CudaDataType, ldb,
-			&beta,
-			C.GetBatchPtrs(), C.CudaDataType, ldc,
-			A.GetLen(0) + A.GetLen(1), CUBLAS_COMPUTE_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
- 
-		break;
-	default:
-		break;
-	}
-
-}
 
 
 int main() {
 	cublasHandle_t handle;
 	cublasCreate(&handle);
 
-	Tensor<float> A(5, 4);
-	Tensor<float> B(4, 3);
-	Tensor<float> C(5, 3);
+	Tensor<double> A(5, 4);
+	Tensor<double> B(3, 4);
+	Tensor<double> C(5, 3);
 
-	float alpha = static_cast<float>(1.0f);
-	float beta = static_cast<float>(0.0f);
+	double alpha = static_cast<double>(1.0f);
+	double beta = static_cast<double>(0.0f);
 
 	A.Fill((2.1f));
-	B.Fill((2));
+	B.Fill((2.0f));
 
-	auto A_chunks = A.Chunk(1, 2);
-	auto B_chunks = B.Chunk(1, 2);
-
-	Matmul(handle, A, B, C, false, false, alpha, beta);
+	Functions::Matmul(handle, A, B, C, false, true, alpha, beta);
 
 	std::cout << "A:" << A.ToString() << std::endl;
 	std::cout << "B:" << B.ToString() << std::endl;
-	std::cout << "C2:" << C.ToString() << std::endl;
+	std::cout << "C:" << C.ToString() << std::endl;
 
 	cublasDestroy(handle);
 
